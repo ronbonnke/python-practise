1e8214dc53dde648ffe3ae93e38de8f10a58fdf7
JAVA-DSA <ronbonnke789@gmail.com>
2024-03-17 00:25:30 +0530

    your message

--- basic.py
+++ basic.py
@@ -1,2 +1,37 @@
+# learn to print the statements and move on to the next step
 print("hello world")
 print('Welcome to python : The world of real snakes')
+
+print("3+6 : " , 3 + 6)
+
+ # division is not equal to integer division. It gives a decimal value
+print("3/6 : ", 3 / 6) 
+
+print("3*6 : ", 3 * 6)
+print("3-6 : ", 3 - 6)
+print("3%6 : ", 3 % 6)
+
+# checking condition
+print("3!=6 : ", 3 != 6)
+
+print("3&6 : ", 3 & 6)
+
+# checking condition
+print("3==6 : ", 3 == 6)
+
+# use power 
+# code explanation  2*2*2 = 8
+print("2 ** 3 : ", 2 ** 3)
+
+
+# you can find out which operator gets the first preference while calculating these (basic Arithmetic operations)
+
+print("2+10*10+3 : ", 2+10*10+3)
+
+print("(2+10)*(10+3) : ", (2+10)*(10+3))
+
+print("2+(10*10)+3 : ", 2+(10*10)+3)
+
+print("(2+10)*10+3 : ", (2+10)*10+3)
+
+print("2+10*(10+3) : ", 2+10*(10+3))
