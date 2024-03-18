ace6ee108e2d8eb9313dc90f70befc6c212178e8
JAVA-DSA <ronbonnke789@gmail.com>
2024-03-18 10:17:12 +0530

    your message

--- basic.py
+++ basic.py
@@ -35,3 +35,74 @@ print("2+(10*10)+3 : ", 2+(10*10)+3)
 print("(2+10)*10+3 : ", (2+10)*10+3)
 
 print("2+10*(10+3) : ", 2+10*(10+3))
+
+
+# eg1
+a = 10
+a = a+a
+print(a+a)
+print(type(a))
+
+# eg2
+a = 30.1
+print(a)
+print(type(a))
+
+# eg3
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+
+
+print('hello')
+print("hello")
+print("I'm Ron")
+
+
+
+myString = "abcdefghijk"
+print(myString[2])
+
+# we use colon to print the rest other numbers starting from the index number 2 which is c.
+#  but here the starting point of the index is included here including the index 
+myString = "abcdefghijk"
+print(myString[2:])
+
+#here it goes upto the index 4 but it doesn't include the 4th index keep that in mind
+myString = "abcdefghijk"
+print(myString[:4])
+
+
+# you can see it goes till the required index in right but gives the previous value
+print(myString[3:6])
+print(myString[2:4])
+
+# lets print all the string "abcdefghijk"
+print(myString[0:11])
+
+# there's one more way to print all strings
+print(myString[::])
+
+# for skipping numbers
+print(myString[::2])
+
+# for skipping from a particular index
+print(myString[2:7:2]) 
+
+
+# --- just a trick ---- #
+# for REVERSING the given string
+print(myString[::-1])
+
+# add
+print(2+3)
+
+# concat
+print('2' + '3')
+
+x = "Hello World"
+print(x.upper())
+print(x.lower())
+
+print(x.split())
